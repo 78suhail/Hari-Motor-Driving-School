425232ccdc71dd08141fccedc53cdb9bd62eb7d7
78suhail <suhail.codinghub@gmail.com>
2025-09-07 00:06:19 +0530

    update

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -147,7 +147,7 @@ const AboutUs = () => {
             </Link>
             <button className="flex items-center justify-center bg-blue-900 text-white px-8 py-3 rounded font-semibold hover:bg-blue-800 transition">
               <FaPhoneAlt className="w-5 h-5 mr-2" />
-              +91 9999 99 4613
+              +91 880-073-3355
             </button>
           </div>
         </div>
